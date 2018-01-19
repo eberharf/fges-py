b0e23d166c3781c2175b94dfaefb4b3a39cb0dae
RAntonello <rjantonel@gmail.com>
2018-01-19 06:00:59 -0800

    Add command line support
    Signed-off-by: RAntonello <rjantonel@gmail.com>

--- runner.py
+++ runner.py
@@ -2,12 +2,13 @@ import numpy as np
 from SEMScore import *
 from fges import *
 import time
+import sys
 
 def load_file(data_file):
     return np.loadtxt(data_file, skiprows = 1)
 
-if __name__ == '__main__':
-    dataset = load_file("data/HCP.tsv")[:,:10]
+def main():
+    dataset = load_file(sys.argv[1])
     score = SEMBicScore(dataset, len(dataset), 2) # Initialize SEMBic Object
     variables = list(range(len(dataset[0])))
     print("Running FGES on graph with " + str(len(variables)) + " nodes.")
@@ -15,3 +16,6 @@ if __name__ == '__main__':
     start_time = time.time()
     fges.search()
     print("--- %s seconds ---" % (time.time() - start_time))
+
+if __name__ == "__main__":
+    main()
