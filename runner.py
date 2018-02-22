a3254aa70156c20fce578d9c06c29799980adb49
RAntonello <rjantonel@gmail.com>
2018-02-21 22:10:12 -0800

    RAM testing
    Signed-off-by: RAntonello <rjantonel@gmail.com>

--- runner.py
+++ runner.py
@@ -3,12 +3,14 @@ from SEMScore import *
 from fges import *
 import time
 import sys
+import joblib
 
 def load_file(data_file):
     return np.loadtxt(data_file, skiprows = 1)
 
 def main():
     dataset = load_file(sys.argv[1])
+    #dataset = joblib.load("/home/ubuntu/PycharmProjects/fges-py/data/zebrafish_whole_brain2.npy")[:,:int(sys.argv[1])]
     score = SEMBicScore(dataset, len(dataset), float(sys.argv[2])) # Initialize SEMBic Object
     variables = list(range(len(dataset[0])))
     print("Running FGES on graph with " + str(len(variables)) + " nodes.")
