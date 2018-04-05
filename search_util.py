a9dbac63c08dd137263a16edd7245079ff970338
Ethan Pronovost <epronovo@caltech.edu>
2018-04-05 15:54:28 -0700

    Improve dagFromPattern
    * Clean up implementation of dagFromPattern
    * Add dagFromPatternWithColliders, which doesn’t assume that we can
    orient all edges without creating new unshielded colliders

--- search_util.py
+++ search_util.py
@@ -4,6 +4,7 @@ import networkx as nx
 from meekrules import MeekRules
 from graph_util import *
 import numpy as np
+import queue
 
 def get_undir_edge(g):
     '''Find an undirected edge in Graph g, or None if none exist'''
@@ -16,7 +17,7 @@ def dagFromPattern(graph):
     '''
     Construct a specific graph from an equivalence class
     :param graph: Equivalence class (i.e. DAG with unoriented edges)
-    :return: Candidate member (ie. DAG with no unoriented edges, a subset of input graph)
+    :return: Candidate dag, or None if unsuccessful
     '''
 
     dag = graph.copy()
@@ -26,45 +27,116 @@ def dagFromPattern(graph):
     if detect_cycle(dag):
         return None
 
-    # Tuples of (graph, last edge oriented, orientation status)
-    # Orientation status: 0 = unoriented, 1 = forward, 2 = backward
-    choices = []
-    choices.append((dag, get_undir_edge(dag), 0))
+    def check_graph(dag, node):
+        '''
+        Check if a dag is okay.
+        :param dag: dag to check
+        :param node: node to check for cycles
+        :return: dag with implied edges if valid, else None
+        '''
+        rules.orient_implied(dag)
+        new_colliders = get_all_collider_triples(dag)
+        if new_colliders == graph_colliders and not detect_cycle_at_node(dag, node):
+            return dag
+        else:
+            return None
 
-    while len(choices) > 0:
-        (g, edge, status) = choices.pop(-1)
+    def try_to_solve(pattern):
+        '''
+        Try to find a fully oriented dag from a pattern using a DFS
+        :param pattern: the pattern to try
+        :return: dag if successful, else None
+        '''
+        if pattern is None:
+            return None
+
+        edge = get_undir_edge(pattern)
 
         if edge is None:
             # No more undirected edges
-            return g
+            return pattern
 
         (node1, node2) = edge
-        new_g = g.copy()
-
-        if (status == 0) and (node2 not in get_ancestors(new_g, node1)):
-            # oreint node1 -> node2
+        new_g = pattern.copy()
+        if node2 not in get_ancestors(new_g, node1):
+            # Orient edge node1 -> node2
             new_g.remove_edge(node2, node1)
-            new_status = 1
-        elif (status <= 1) and (node1 not in get_ancestors(new_g, node2)):
-            # orient node2 -> node1
+            result = try_to_solve(check_graph(new_g, node1))
+            if result is not None:
+                return result
+
+        new_g = pattern.copy()
+        if node1 not in get_ancestors(new_g, node2):
+            # Orient edge node2 -> node1
             new_g.remove_edge(node1, node2)
-            if not new_g.has_edge(node2, node1):
-                new_g.add_edge(node2, node1)
-            new_status = 2
-        else:
-            # Go back to prior decision
+            result = try_to_solve(check_graph(new_g, node2))
+            if result is not None:
+                return result
+
+        return None
+
+    return try_to_solve(graph)
+
+#TODO: use smarter penalty function than simply a count
+def dagFromPatternWithColliders(graph):
+    '''
+    Construct a DAG from the pattern graph that minimizes the number of new colliders added.
+    :param graph: The pattern to use
+    :return: DAG
+    '''
+    pattern = graph.copy()
+    original_colliders = get_all_collider_triples(pattern)
+
+    optimal_graph = None
+    optimal_penalty = np.inf
+
+    stack = queue.LifoQueue()
+    stack.put((pattern, original_colliders, 0))
+
+    while not stack.empty():
+        g, colliders, penalty = stack.get()
+        if penalty >= optimal_penalty:
+            # Branch and Bound
             continue
 
-        rules.orient_implied(new_g)
+        found_undirected = False
+        # Consider all undirected edges
+        for (x, y) in g.edges():
+            if not has_undir_edge(g, x, y) or y < x:
+                continue
 
-        new_colliders = get_all_collider_triples(new_g)
-        if new_colliders == graph_colliders and find_directed_path(new_g, node1, node1) is None:
-            # Commit to orientation for time being
-            choices.append((new_g, edge, new_status))
-            choices.append((new_g, get_undir_edge(new_g), 0))
-        else:
-            # Try other orientation
-            choices.append((g, edge, new_status))
+            found_undirected = True
+
+            # Orient x -> y
+            g1 = g.copy()
+            g1.remove_edge(y, x)
+
+            if not detect_cycle_at_node(g1, x):
+                c1 = check_for_colliders(g1, y)
+                p1 = penalty + len(c1 - colliders) # set difference
+                e1 = g1.edges()
+                stack.put((g1, colliders.union(c1), p1))
+
+            # Orient y -> x
+            g2 = g.copy()
+            g2.remove_edge(x, y)
+
+            if not detect_cycle_at_node(g2, y):
+                c2 = check_for_colliders(g2, x)
+                p2 = penalty + len(c2 - colliders)  # set difference
+                e2 = g2.edges()
+                stack.put((g2, colliders.union(c2), p2))
+
+        if not found_undirected:
+            # Completely oriented graph
+            if penalty < optimal_penalty:
+                optimal_graph = g
+                optimal_penalty = penalty
+                if penalty == 0:
+                    break
+
+    return (optimal_graph, optimal_penalty)
+
 
 def mean_shift_data(data):
     '''Shift all variables in a dataset to have mean zero'''
@@ -104,7 +176,7 @@ def estimate_parameters(dag, data):
             edge_parameters[j, inbound_nodes[i]] = params[i]
             # v = edge_parameters * v + e
 
-    return edge_parameters, residuals
+    return np.array(edge_parameters), np.array(residuals)
 
 def get_covariance_matrix(params, resids):
     '''
--- tests/test_search_util.py
+++ tests/test_search_util.py
@@ -71,6 +71,113 @@ class TestDagFromPattern(unittest.TestCase):
         assert is_def_collider(dag, 1, 2, 3)
         assert not is_def_collider(dag, 2, 3, 4)
 
+    def test_cycle(self):
+        '''
+        X0 -- X1 -- X2 -- X3 -- X0
+        '''
+        graph = nx.DiGraph()
+        graph.add_nodes_from([0, 1, 2, 3])
+        graph.add_edges_from([(0, 1), (1, 0),
+                              (1, 2), (2, 1),
+                              (2, 3), (3, 2),
+                              (3, 0), (0, 3)])
+        dag = dagFromPattern(graph)
+        assert dag is None
+
+class TestDagFromPatternWithColliders(unittest.TestCase):
+
+    def test_triple(self):
+        '''
+        X0 -- X1 -- X2
+        Resulting graph shouldn't have X1 be a collider
+        '''
+        graph = nx.DiGraph()
+        graph.add_nodes_from([0, 1, 2])
+        graph.add_edges_from([(0, 1), (1, 0), (1, 2), (2, 1)])
+
+        dag, penalty = dagFromPatternWithColliders(graph)
+
+        assert dag is not None
+        assert penalty == 0
+        assert get_undir_edge(dag) is None
+        assert not is_def_collider(dag, 0, 1, 2)
+
+    def test_cross(self):
+        '''X0 -- X1 -- X2 and X3 -- X1 -- X4'''
+        graph = nx.DiGraph()
+        graph.add_nodes_from([0, 1, 2, 3, 4])
+        graph.add_edges_from([(0, 1), (1, 0),
+                              (1, 2), (2, 1),
+                              (3, 1), (1, 3),
+                              (1, 4), (4, 1)])
+
+        dag, penalty = dagFromPatternWithColliders(graph)
+
+        assert dag is not None
+        assert penalty == 0
+
+        assert get_undir_edge(dag) is None
+        for i in [0, 2, 3, 4]:
+            for j in [0, 2, 3, 4]:
+                if i != j:
+                    assert not is_def_collider(dag, i, 1, j)
+
+    def test_undo(self):
+        '''
+        X0 --- X1 <-- X2
+        The algorithm will first try X0 --> X1, then have to
+          retry since this creates a collider.
+        '''
+        graph = nx.DiGraph()
+        graph.add_nodes_from([0, 1, 2])
+        graph.add_edges_from([(0, 1), (1, 0), (2, 1)])
+
+        dag, penalty = dagFromPatternWithColliders(graph)
+
+        assert dag is not None
+        assert penalty == 0
+
+        assert get_undir_edge(dag) is None
+        assert not is_def_collider(dag, 0, 1, 2)
+        assert has_dir_edge(dag, 1, 0)
+
+    def test_several(self):
+        '''
+        X0 --- X1 --> X2 <-- X3 --- X4
+        '''
+        graph = nx.DiGraph()
+        graph.add_nodes_from([0, 1, 2, 3, 4])
+        graph.add_edges_from([(0, 1), (1, 0),
+                              (1, 2),
+                              (3, 2),
+                              (3, 4), (4, 3)])
+
+        dag, penalty = dagFromPatternWithColliders(graph)
+
+        assert dag is not None
+        assert penalty == 0
+
+        assert get_undir_edge(dag) is None
+        assert not is_def_collider(dag, 0, 1, 2)
+        assert is_def_collider(dag, 1, 2, 3)
+        assert not is_def_collider(dag, 2, 3, 4)
+
+    def test_cycle(self):
+        '''
+        X0 -- X1 -- X2 -- X3 -- X0
+        '''
+        graph = nx.DiGraph()
+        graph.add_nodes_from([0, 1, 2, 3])
+        graph.add_edges_from([(0, 1), (1, 0),
+                              (1, 2), (2, 1),
+                              (2, 3), (3, 2),
+                              (3, 0), (0, 3)])
+        dag, penalty = dagFromPatternWithColliders(graph)
+
+        assert dag is not None
+        assert penalty == 1
+        assert not detect_cycle(dag)
+
 class TestEstimateParameters(unittest.TestCase):
 
     def test_params_1(self):
