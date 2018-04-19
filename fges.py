967759b0f6639c84e22e64b6f9ca53d4e107c654
Ethan Pronovost <epronovo@caltech.edu>
2018-04-19 15:59:30 -0700

    Remove extra )

--- fges.py
+++ fges.py
@@ -177,7 +177,7 @@ class FGES:
 
             if (not (arrow.na_y_x == graph_util.get_na_y_x(self.graph, x, y))) or\
                     (not graph_util.adjacent(self.graph, x, y)) or (graph_util.has_dir_edge(self.graph, y, x) or\
-                         graph_util.has_undir_edge(self.graph, x, y))):
+                         graph_util.has_undir_edge(self.graph, x, y)):
                 continue
 
             diff = set(arrow.na_y_x)
