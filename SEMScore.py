9d733b9b926cfd9fecc07e08b4b41b7ff0e456f3
RAntonello <rjantonel@gmail.com>
2018-07-11 20:54:47 -0700

    Improve speed by moving to recursive Pearson correlation def

--- SEMScore.py
+++ SEMScore.py
@@ -14,6 +14,8 @@ class SEMBicScore:
         self.cov = np.cov(dataset, rowvar=False)
         self.sample_size = len(dataset)
         self.penalty = penalty_discount
+        self.corrcoef = np.corrcoef(np.swapaxes(dataset,0,1))
+        self.cache = {}
 
     def partial_corr(self, x, y, Z):
         """
@@ -33,6 +35,30 @@ class SEMBicScore:
 
         corr = np.corrcoef(res_i, res_j)
         return corr.item(0, 1)
+
+    def recursive_partial_corr(self, x, y, Z):
+        if Z == []:
+            return self.corrcoef[x][y]
+        else:
+            if (x, y, tuple(Z[1:])) not in self.cache:
+                term1 = self.recursive_partial_corr(x, y, Z[1:])
+                if Z[1:] != []:
+                    self.cache[(x, y, tuple(Z[1:]))] = term1
+            else:
+                term1 = self.cache[(x, y, tuple(Z[1:]))]
+            if (x, Z[0], tuple(Z[1:])) not in self.cache:
+                term2 = self.recursive_partial_corr(x, Z[0], Z[1:])
+                if Z[1:] != []:
+                    self.cache[(x, Z[0], tuple(Z[1:]))] = term2
+            else:
+                term2 = self.cache[(x, Z[0], tuple(Z[1:]))]
+            if (y, Z[0], tuple(Z[1:])) not in self.cache:
+                term3 = self.recursive_partial_corr(y, Z[0], Z[1:])
+                if Z[1:] != []:
+                    self.cache[(y, Z[0], tuple(Z[1:]))] = term3
+            else:
+                term3 = self.cache[(y, Z[0], tuple(Z[1:]))]
+            return (term1 - (term2 * term3)) / math.sqrt((1 - (term2 * term2)) * (1 - (term3 * term3)))
 
     def local_score(self, node, parents):
         """ `node` is an int index """
@@ -81,13 +107,13 @@ class SEMBicScore:
 
     def local_score_diff_parents(self, node1, node2, parents):
         #print(node1, node2, parents)
-        r = self.partial_corr(node1, node2, parents)
+        r = self.recursive_partial_corr(node1, node2, parents)
         #print(r)
         return -self.sample_size * math.log(1.0 - r * r) - (len(parents) + 2) * self.penalty * math.log(self.sample_size)
         #return self.local_score(node2, parents + [node1]) - self.local_score(node2, parents)
 
     def local_score_diff(self, node1, node2):
-        r = self.partial_corr(node1, node2, [])
+        r = self.recursive_partial_corr(node1, node2, [])
         #print(r)
         return -self.sample_size * math.log(1.0 - r * r)
         #return self.local_score(node2, [node1]) - self.local_score(node2, [])
